d6a3a53b9f64de77cf466eae01f80bb1c08b34e9
Marcel Ebert <mail@marcel-ebert.de>
2021-11-03 15:33:41 +0100

    Fetch contract state

--- src/components/AMM/index.tsx
+++ src/components/AMM/index.tsx
@@ -1,6 +1,11 @@
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import uiKeyring from '@polkadot/ui-keyring';
 import BigNumber from 'big.js';
+import React from 'react';
+import { useGlobalState } from '../../GlobalStateProvider';
+import PendulumApi from '../../lib/api';
 import DepositView from './Deposit';
 import ReservesView from './Reserves';
 import SwapView from './Swap';
@@ -10,20 +15,50 @@ export const AMM_ASSETS = [
   { code: 'EUR', issuer: 'GAKNDFRRWA3RPWNLTI3G4EBSD3RGNZZOY5WKWYMQ6CQTG3KIEKPYWAYC' }
 ];
 
-export const AMM_LP_TOKEN_CODE = 'USDC_EUR_LPT';
+export const AMM_LP_TOKEN_CODE = 'USDC-EUR LP';
 
 export type BalancePair = [BigNumber, BigNumber];
 
 function AmmView() {
-  const reserves: BalancePair = [BigNumber(0), BigNumber(0)];
-  const poolTokenTotal = BigNumber(0);
+  const { state } = useGlobalState();
+
+  const [reserves, setReserves] = React.useState<BalancePair>([BigNumber(0), BigNumber(0)]);
+  const [totalSupply, setTotalSupply] = React.useState<BigNumber>(BigNumber(0));
+
+  const contract = React.useMemo(() => {
+    try {
+      const userKeypair = uiKeyring.keyring.pairs.find((pair) => pair.address === state.accountExtraData?.address);
+      if (userKeypair) {
+        userKeypair.unlock(undefined);
+        return PendulumApi.get().getAMMContract(userKeypair);
+      } else {
+        return undefined;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, [state.accountExtraData]);
+
+  React.useEffect(() => {
+    if (contract) {
+      contract.getReserves().then(setReserves);
+      contract.getTotalSupply().then(setTotalSupply);
+    }
+  }, [contract]);
+
   return (
     <Container maxWidth='md' component='main'>
-      <ReservesView reserves={reserves} />
-      <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
-      <DepositView reserves={reserves} poolTokenTotal={poolTokenTotal} />
-      <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
-      <SwapView />
+      {contract ? (
+        <>
+          <ReservesView reserves={reserves} />
+          <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+          <DepositView deposit={contract.depositAsset} reserves={reserves} poolTokenTotal={totalSupply} />
+          <Divider sx={{ marginTop: 2, marginBottom: 2 }} />
+          <SwapView />
+        </>
+      ) : (
+        <Typography variant='h4'>Could not instantiate AMM contract.</Typography>
+      )}
     </Container>
   );
 }
