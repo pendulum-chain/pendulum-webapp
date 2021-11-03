eaeb376e115d422afa2333dd45a424e9c0cb9f88
Marcel Ebert <mail@marcel-ebert.de>
2021-11-03 15:34:10 +0100

    Implement deposit functionality

--- src/components/AMM/Deposit.tsx
+++ src/components/AMM/Deposit.tsx
@@ -10,38 +10,38 @@ import { usePromiseTracker } from '../../lib/promises';
 import AssetSelector from '../AssetSelector';
 import AssetTextField from '../AssetTextField';
 
-function calculateDeposit(asset: Asset, amount: string, reserves: BalancePair, poolTokenTotal: BigNumber) {
-  const depositIntent = {
-    amount: BigNumber(amount),
-    asset
-  };
+const MINIMUM_LIQUIDITY = 1; // TODO fetch from AMM
 
-  const depositAmounts = [
-    assetEquals(depositIntent.asset, AMM_ASSETS[0])
-      ? depositIntent.amount
-      : depositIntent.amount.mul(reserves[0]).div(reserves[1]).round(7),
-    assetEquals(depositIntent.asset, AMM_ASSETS[0])
-      ? depositIntent.amount.mul(reserves[1]).div(reserves[0]).round(7)
-      : depositIntent.amount
-  ] as const;
+function calculateDeposit(asset: Asset, amount: BigNumber, reserves: BalancePair, poolTokenTotal: BigNumber) {
+  const [amount0, amount1] = assetEquals(asset, AMM_ASSETS[0])
+    ? [amount, reserves[0].gt(0) ? amount.times(reserves[1]).div(reserves[0]) : amount]
+    : [reserves[1].gt(0) ? amount.times(reserves[0]).div(reserves[1]) : amount, amount];
 
-  const liquidityTokens = depositAmounts[0].mul(poolTokenTotal).div(reserves[0]);
+  const liquidity = poolTokenTotal.eq(0)
+    ? amount0.times(amount1).sqrt().minus(MINIMUM_LIQUIDITY)
+    : BigNumber(
+        Math.min(
+          amount0.times(poolTokenTotal).div(reserves[0]).toNumber(),
+          amount1.times(poolTokenTotal).div(reserves[1]).toNumber()
+        )
+      );
 
   const deposit = {
-    depositAmounts,
-    liquidityTokens: liquidityTokens.round(2, 0 /* round down */)
+    depositAmounts: { amount0, amount1 },
+    liquidityTokens: liquidity.round(2, 0 /* round down */)
   } as const;
 
   return deposit;
 }
 
 interface Props {
+  deposit: (amount: string, depositAsset1: boolean) => Promise<void>;
   reserves: BalancePair;
   poolTokenTotal: BigNumber;
 }
 
 function DepositView(props: Props) {
-  const { reserves: balancePair, poolTokenTotal } = props;
+  const { deposit, reserves, poolTokenTotal } = props;
 
   const selectableAssets = AMM_ASSETS;
 
@@ -55,12 +55,12 @@ function DepositView(props: Props) {
   React.useEffect(() => {
     if (userAmount) {
       try {
-        const result = calculateDeposit(asset1, userAmount, balancePair, poolTokenTotal);
+        const result = calculateDeposit(asset1, BigNumber(userAmount), reserves, poolTokenTotal);
 
         if (assetEquals(asset1, AMM_ASSETS[0])) {
-          setCalculatedAmount(String(result.depositAmounts[1]));
+          setCalculatedAmount(String(result.depositAmounts.amount1));
         } else {
-          setCalculatedAmount(String(result.depositAmounts[0]));
+          setCalculatedAmount(String(result.depositAmounts.amount0));
         }
 
         setEstimatedLPT(String(result.liquidityTokens));
@@ -71,9 +71,15 @@ function DepositView(props: Props) {
       setCalculatedAmount('');
       setEstimatedLPT('');
     }
-  }, [userAmount, asset1, balancePair, poolTokenTotal]);
+  }, [userAmount, asset1, reserves, poolTokenTotal]);
 
-  const onProvideClick = React.useCallback(() => {}, []);
+  const onProvideClick = React.useCallback(() => {
+    if (assetEquals(asset1, AMM_ASSETS[0])) {
+      submission.track(deposit(userAmount, true).catch(console.error)).catch(console.error);
+    } else {
+      submission.track(deposit(userAmount, false).catch(console.error)).catch(console.error);
+    }
+  }, [asset1, deposit, userAmount, submission]);
 
   return (
     <Box display='flex' flexDirection='column' alignItems='center'>
@@ -118,7 +124,7 @@ function DepositView(props: Props) {
       )}
       <Button
         color='primary'
-        disabled={!userAmount}
+        disabled={!userAmount || submission.state === 'pending'}
         startIcon={submission.state === 'pending' ? <CircularProgress size={16} /> : null}
         variant='outlined'
         style={{ marginTop: 16 }}
