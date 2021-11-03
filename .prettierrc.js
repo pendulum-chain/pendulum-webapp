645c2377be3fa7b8cb19d40964d5f6326fdd32f3
Marcel Ebert <mail@marcel-ebert.de>
2021-11-03 17:02:46 +0100

    Add prettier and lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  jsxSingleQuote: true,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+  useTabs: false
+};
