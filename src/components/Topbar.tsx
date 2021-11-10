700c2f6df5c2960f649c17b91d0e0a7b168cbef1
Gonza <gonzalo@satoshipay.io>
2021-11-10 20:57:37 +0100

    adjust header size

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -10,7 +10,7 @@ const useStyles = makeStyles((theme) => ({
   appBar: {
     boxShadow: 'none',
     backgroundColor: '#ffffff',
-    height: '90px',
+    height: '80px',
     justifyContent: 'center'
   }
 }));
