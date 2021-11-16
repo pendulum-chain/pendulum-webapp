888b86fa1b09ac0dfa70b15adbe707f8640ee438
Gonza <gonzalo@satoshipay.io>
2021-11-16 16:01:03 +0100

    Add faucet to the PEN balances card

--- src/components/BalanceCard.tsx
+++ src/components/BalanceCard.tsx
@@ -4,16 +4,34 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Tooltip from '@mui/material/Tooltip';
 import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
+
 import Typography from '@mui/material/Typography';
 
 import PendulumApi from '../lib/api';
 import { Balance } from './Balances';
 import { useGlobalState } from '../GlobalStateProvider';
+import Faucet from '../lib/faucet';
+
+const MIN_BALANCE = 100;
 
 export default function BalanceCard(props: any) {
   const { state } = useGlobalState();
   let { balance: prevBalance } = props;
   const [newBalance, setNewBalance] = useState<Balance>(prevBalance);
+  const [loading, setLoading] = useState(false);
+
+  const isNative = prevBalance.asset === 'PEN';
+  const shouldGetMoreTokens = parseInt(newBalance.free) < MIN_BALANCE;
+
+  const getPenTokensFromFaucet = async () => {
+    const faucet = new Faucet();
+    if (state.accountExtraData?.address && shouldGetMoreTokens) {
+      setLoading(true);
+      await faucet.send(state.accountExtraData?.address, MIN_BALANCE);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     async function bind() {
@@ -42,16 +60,37 @@ export default function BalanceCard(props: any) {
         <Typography sx={{ color: '#aaa' }}>{newBalance.frozen} frozen</Typography>
       </CardContent>
       <CardActions>
-        <Tooltip title='Bridge to Stellar' arrow>
-          <Button fullWidth variant='outlined'>
-            Send
-          </Button>
-        </Tooltip>
-        <Tooltip title='Bridge to Stellar' arrow>
-          <Button fullWidth variant='contained'>
-            Receive
-          </Button>
-        </Tooltip>
+        {isNative ? (
+          <Tooltip
+            title={`If your balance is low enough (less than ${MIN_BALANCE} PEN), you can always ask the Pendulum faucet to give you some more tokens.`}
+            arrow
+          >
+            <span style={{ width: '100%' }}>
+              <LoadingButton
+                disabled={!shouldGetMoreTokens}
+                loading={loading}
+                onClick={() => getPenTokensFromFaucet()}
+                fullWidth
+                variant='outlined'
+              >
+                Get tokens
+              </LoadingButton>
+            </span>
+          </Tooltip>
+        ) : (
+          <>
+            <Tooltip title='Bridge to Stellar' arrow>
+              <Button fullWidth variant='outlined'>
+                Send
+              </Button>
+            </Tooltip>
+            <Tooltip title='Bridge to Stellar' arrow>
+              <Button fullWidth variant='contained'>
+                Receive
+              </Button>
+            </Tooltip>
+          </>
+        )}
       </CardActions>
     </Card>
   );
