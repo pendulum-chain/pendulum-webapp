5873a2bf6ac6e35f0a609c724a27134285931a74
Marcel Ebert <mail@marcel-ebert.de>
2022-02-17 18:33:33 +0100

    Remove unused parts of craco.config.js

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,8 @@
-const { throwUnexpectedConfigError } = require('@craco/craco');
-
 const { ProvidePlugin } = require('webpack');
 
 module.exports = {
   reactScriptsVersion: 'react-scripts' /* (default value) */,
 
-  babel: {
-    presets: [],
-    plugins: [],
-    loaderOptions: (babelLoaderOptions, { env, paths }) => {
-      return babelLoaderOptions;
-    }
-  },
-  typescript: {
-    enableTypeChecking: true /* (default value)  */
-  },
-
-  jest: {
-    babel: {
-      addPresets: true /* (default value) */,
-      addPlugins: true /* (default value) */
-    },
-    configure: (jestConfig, { env, paths, resolve, rootDir }) => {
-      return jestConfig;
-    }
-  },
   webpack: {
     configure: (webpackConfig, { env, paths }) => {
       webpackConfig.resolve.fallback = {
@@ -38,32 +16,5 @@ module.exports = {
       return webpackConfig;
     },
     plugins: [new ProvidePlugin({ Buffer: ['buffer', 'Buffer'] })]
-  },
-  devServer: (devServerConfig, { env, paths, proxy, allowedHost }) => {
-    return devServerConfig;
-  },
-  plugins: [
-    {
-      plugin: {
-        overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => {
-          return cracoConfig;
-        },
-        overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env, paths } }) => {
-          return webpackConfig;
-        },
-        overrideDevServerConfig: ({
-          devServerConfig,
-          cracoConfig,
-          pluginOptions,
-          context: { env, paths, proxy, allowedHost }
-        }) => {
-          return devServerConfig;
-        },
-        overrideJestConfig: ({ jestConfig, cracoConfig, pluginOptions, context: { env, paths, resolve, rootDir } }) => {
-          return jestConfig;
-        }
-      },
-      options: {}
-    }
-  ]
+  }
 };
