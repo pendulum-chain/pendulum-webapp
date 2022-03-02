a456110f3b82d3573450c50bbe54157cd8ba83c0
Marcel Ebert <mail@marcel-ebert.de>
2022-03-02 10:30:33 +0100

    Change NodeSelection component

--- src/components/NodeSelection/index.tsx
+++ src/components/NodeSelection/index.tsx
@@ -1,3 +1,5 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { LoadingButton } from '@mui/lab';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Drawer from '@mui/material/Drawer';
@@ -5,13 +7,12 @@ import FormControl from '@mui/material/FormControl';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 import { useGlobalState } from '../../GlobalStateProvider';
-import { getDefaultNode, knownNodes, Node, setDefaultNode } from '../../lib/nodes';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
 import PendulumApi from '../../lib/api';
+import { getDefaultNode, knownNodes, Node, setDefaultNode } from '../../lib/nodes';
 
 interface NodeSelectionProps {
   onSave: () => void;
@@ -21,9 +22,12 @@ function NodeSelection(props: NodeSelectionProps) {
   const { state, setState } = useGlobalState();
   const [selectedNode, setSelectedNode] = React.useState<Node>(state.currentNode || getDefaultNode());
 
-  const [customNodeURL, setCustomNodeURL] = React.useState<string>('ws://localhost:8844');
+  const [customNodeInputError, setCustomNodeInputError] = React.useState<string | null>(null);
+  const [customNodeEndpoint, setCustomNodeEndpoint] = React.useState<string>('ws://localhost:8844');
   const [customAMMAddress, setCustomAMMAddress] = React.useState<string>(selectedNode.amm_address);
   const [showCustomNodeField, setShowCustomNodeField] = React.useState(selectedNode?.display_name === 'Custom');
+
+  const [loading, setLoading] = React.useState(false);
 
   const handleChange = (event: SelectChangeEvent) => {
     const newNodeDisplayName = event.target.value;
@@ -33,25 +37,65 @@ function NodeSelection(props: NodeSelectionProps) {
       setSelectedNode(existingNode);
     } else {
       setShowCustomNodeField(true);
-      setSelectedNode({ display_name: 'Custom', url: customNodeURL, amm_address: customAMMAddress });
+      setSelectedNode({ display_name: 'Custom', wss_endpoint: customNodeEndpoint, amm_address: customAMMAddress });
     }
   };
 
-  const onSave = async () => {
-    if (selectedNode?.display_name === 'Custom') {
-      const customNode = { ...selectedNode, url: customNodeURL, amm_address: customAMMAddress };
-      // await initialization before changing global state because
-      // other components listen to state and would use potentially outdated api
-      await PendulumApi.get().init(customNode.url);
-      setState({ ...state, currentNode: customNode });
-      setDefaultNode(customNode);
-    } else if (selectedNode) {
-      await PendulumApi.get().init(selectedNode.url);
-      setDefaultNode(selectedNode);
-      setState({ ...state, currentNode: selectedNode });
+  const handleCustomNodeEndpointChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const url = event.target.value;
+    if (!url.startsWith('ws://') && !url.startsWith('wss://')) {
+      setCustomNodeInputError('Invalid WSS endpoint URL');
+    } else {
+      setCustomNodeInputError(null);
     }
 
-    props.onSave();
+    setCustomNodeEndpoint(url);
+  };
+
+  const onSave = async () => {
+    setLoading(true);
+    if (selectedNode?.display_name === 'Custom') {
+      const customNode = { ...selectedNode, url: customNodeEndpoint, amm_address: customAMMAddress };
+      // await initialization before changing global state because
+      // other components listen to state and would use potentially outdated api
+      try {
+        await PendulumApi.get().init(customNode.url);
+
+        setState({
+          ...state,
+          currentNode: customNode,
+          toast: { message: `Connected to ${customNode.url}`, type: 'success' }
+        });
+        setDefaultNode(customNode);
+        props.onSave();
+      } catch (error) {
+        // change currentNode even on error to prevent inconsistency
+        setState({
+          ...state,
+          currentNode: customNode,
+          toast: { message: `Failed to connect to ${customNode.url}`, type: 'error' }
+        });
+      }
+    } else if (selectedNode) {
+      try {
+        await PendulumApi.get().init(selectedNode.wss_endpoint);
+        setDefaultNode(selectedNode);
+        setState({
+          ...state,
+          currentNode: selectedNode,
+          toast: { message: `Connected to ${selectedNode.wss_endpoint}`, type: 'success' }
+        });
+        props.onSave();
+      } catch (error) {
+        // change currentNode even on error to prevent inconsistency
+        setState({
+          ...state,
+          currentNode: selectedNode,
+          toast: { message: `Failed to connect to ${selectedNode.wss_endpoint}`, type: 'error' }
+        });
+      }
+    }
+    setLoading(false);
   };
 
   return (
@@ -80,11 +124,13 @@ function NodeSelection(props: NodeSelectionProps) {
         {showCustomNodeField && (
           <>
             <TextField
-              label='Custom Node URL'
-              onChange={(e) => setCustomNodeURL(e.target.value)}
-              value={customNodeURL}
+              label='Custom Node Endpoint'
+              error={Boolean(customNodeInputError)}
+              helperText={customNodeInputError}
+              onChange={handleCustomNodeEndpointChange}
+              value={customNodeEndpoint}
               sx={{ marginTop: 2 }}
-              placeholder='ws://...'
+              placeholder='wss://...'
             />
             <TextField
               label='AMM Address (optional)'
@@ -95,9 +141,14 @@ function NodeSelection(props: NodeSelectionProps) {
             />
           </>
         )}
-        <Button onClick={onSave} sx={{ marginTop: 1 }}>
+        <LoadingButton
+          disabled={Boolean(customNodeInputError)}
+          loading={loading}
+          onClick={onSave}
+          sx={{ marginTop: 1 }}
+        >
           Save
-        </Button>
+        </LoadingButton>
       </FormControl>
     </Box>
   );
